5ef2b8a6072b3bdf3a9f7c1ad4feaf23d769aa56
Mikhail Shulga <mmshulga@gmail.com>
2018-11-11 22:44:07 +0300

    Initial commit, structure prepared.

---
+++ src/main/java/my/mmshulga/chucknorrisjokesapp/ChuckNorrisJokesAppApplication.java
@@ -0,0 +1,12 @@
+package my.mmshulga.chucknorrisjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesAppApplication.class, args);
+    }
+}
