c387c1a1a3cbd0f11edf013e3e24a4ae44beb2e7
Mikhail Shulga <mmshulga@gmail.com>
2018-11-12 23:12:38 +0300

    Added a bean configuration and refactored service to use it.

--- src/main/java/my/mmshulga/chucknorrisjokesapp/service/RandomQuoteServiceImpl.java
+++ src/main/java/my/mmshulga/chucknorrisjokesapp/service/RandomQuoteServiceImpl.java
@@ -1,15 +1,21 @@
 package my.mmshulga.chucknorrisjokesapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomQuoteServiceImpl implements RandomQuoteService {
 
-    private static final ChuckNorrisQuotes cnq = new ChuckNorrisQuotes();
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Autowired
+    public RandomQuoteServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
 
     @Override
     public String getRandomQuote() {
-        return cnq.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
